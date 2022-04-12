cd26d7d91eaec1a7d3e4fef69abe55019c071103
Rafael <rafael.gcaldas01@gmail.com>
2022-04-11 22:51:29 -0300

    Home page

---
+++ web/src/pages/app/index.tsx
@@ -0,0 +1,19 @@
+import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0"
+import { GetServerSideProps } from "next";
+
+export default function Home() {
+  const { user } = useUser();
+  
+  return (
+    <div>
+      <h1>Ok</h1>
+
+      <pre>
+        {JSON.stringify(user, null ,2)}
+      </pre>
+
+    </div>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = withPageAuthRequired();
