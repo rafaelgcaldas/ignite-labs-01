1d052d7ccf627a90083ef95b22d6b4bd44a1f143
Rafael <rafael.gcaldas01@gmail.com>
2022-04-20 00:01:43 -0300

    Purchases - create param decorator current-user

--- purchases/src/http/graphql/resolvers/purchases.resolver.ts
+++ purchases/src/http/graphql/resolvers/purchases.resolver.ts
@@ -1,9 +1,11 @@
 import { UseGuards } from "@nestjs/common";
 import { Args, Mutation, Parent, Query, ResolveField, Resolver } from "@nestjs/graphql";
+import { CurrentUser } from "src/http/auth/current-user";
 
 import { ProductsService } from "src/services/products.service";
 import { PurchasesService } from "../../../services/purchases.service";
 import { AuthorizationGuard } from "../../auth/authorization.guard";
+import { CreatePurchaseInput } from "../inputs/create-purchase.input";
 import { Product } from "../models/product";
 import { Purchase } from "../models/purchase";
 
@@ -27,7 +29,13 @@ export class PurchasesResolver {
   }
 
   @Mutation(() => Purchase)
-  createPurchase() {}
+  createPurchase(@Args('data') data: CreatePurchaseInput, @CurrentUser() user) {
+    return null;
+    // return this.purchasesService.createPurchase({
+    //   productId: data.productId,
+
+    // })
+  }
 
 
 }
