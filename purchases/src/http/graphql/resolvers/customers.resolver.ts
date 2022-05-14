deb4f689744ff545f9584e9d77b1dbca6c4d92d4
Rafael <rafael.gcaldas01@gmail.com>
2022-05-13 23:45:32 -0300

    Purchases - microservice reference

--- purchases/src/http/graphql/resolvers/customers.resolver.ts
+++ purchases/src/http/graphql/resolvers/customers.resolver.ts
@@ -1,5 +1,5 @@
 import { UseGuards } from "@nestjs/common";
-import { Parent, Query, ResolveField, Resolver } from "@nestjs/graphql";
+import { Parent, Query, ResolveField, Resolver, ResolveReference } from "@nestjs/graphql";
 
 import { AuthUser, CurrentUser } from "src/http/auth/current-user";
 import { CustomersService } from "src/services/customers.service";
@@ -26,6 +26,11 @@ export class customersResolver {
   @ResolveField(() => Purchase)
   purchases(@Parent() customer: Customer) {
     return this.purchasesService.listAllFromCustomer(customer.id)
+  }
+
+  @ResolveReference()
+  resolveReference(reference: { authUserId: string }) {
+    return this.customersService.getCustomerByAuthUserId(reference.authUserId);
   }
 
 }
