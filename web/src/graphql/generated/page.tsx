252bb6eb88114186c88176b8318b4643fe72b03b
Rafael <rafael.gcaldas01@gmail.com>
2022-06-09 23:20:44 -0300

    WithPublicApollo

--- web/src/graphql/generated/page.tsx
+++ web/src/graphql/generated/page.tsx
@@ -42,38 +42,3 @@ export const ssrMe = {
       withPage: withPageMe,
       usePage: useMe,
     }
-export async function getServerPageGetProducts
-    (options: Omit<Apollo.QueryOptions<Types.GetProductsQueryVariables>, 'query'>, ctx: ApolloClientContext ){
-        const apolloClient = getApolloClient(ctx);
-        
-        const data = await apolloClient.query<Types.GetProductsQuery>({ ...options, query: Operations.GetProductsDocument });
-        
-        const apolloState = apolloClient.cache.extract();
-
-        return {
-            props: {
-                apolloState: apolloState,
-                data: data?.data,
-                error: data?.error ?? data?.errors ?? null,
-            },
-        };
-      }
-export const useGetProducts = (
-  optionsFunc?: (router: NextRouter)=> QueryHookOptions<Types.GetProductsQuery, Types.GetProductsQueryVariables>) => {
-  const router = useRouter();
-  const options = optionsFunc ? optionsFunc(router) : {};
-  return useQuery(Operations.GetProductsDocument, options);
-};
-export type PageGetProductsComp = React.FC<{data?: Types.GetProductsQuery, error?: Apollo.ApolloError}>;
-export const withPageGetProducts = (optionsFunc?: (router: NextRouter)=> QueryHookOptions<Types.GetProductsQuery, Types.GetProductsQueryVariables>) => (WrappedComponent:PageGetProductsComp) : NextPage  => (props) => {
-                const router = useRouter()
-                const options = optionsFunc ? optionsFunc(router) : {};
-                const {data, error } = useQuery(Operations.GetProductsDocument, options)    
-                return <WrappedComponent {...props} data={data} error={error} /> ;
-                   
-            }; 
-export const ssrGetProducts = {
-      getServerPage: getServerPageGetProducts,
-      withPage: withPageGetProducts,
-      usePage: useGetProducts,
-    }
---
+++ web/src/lib/withPublicApollo.tsx
@@ -0,0 +1,33 @@
+import { GetServerSidePropsContext, NextPage } from "next";
+
+import { ApolloClient, ApolloProvider, createHttpLink, from, InMemoryCache, NormalizedCacheObject } from "@apollo/client";
+
+
+export type ApolloClientContext = GetServerSidePropsContext;
+
+export const withPublicApollo = (Component: NextPage) => {
+ return function Provider(props: any) {
+   return (
+    <ApolloProvider client={getApolloClient(undefined, props.apolloState)}>
+      <Component {...props} />
+    </ApolloProvider>
+   )
+ }
+}
+
+export function getApolloClient(
+  ctx?: ApolloClientContext,
+  ssrCache?: NormalizedCacheObject
+) {
+  const httpLink = createHttpLink({
+    uri: 'http://localhost:3332/graphql',
+    fetch
+  })
+  
+  const cache = new InMemoryCache().restore(ssrCache ?? {});
+  
+  return new ApolloClient({
+    link: from([httpLink]),
+    cache
+  })
+}
